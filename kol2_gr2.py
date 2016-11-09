2da92a00a0e1ab7be0ef661dbf67c0474d33d0b7
Krzysztof Szydłowski <kriszna14@gmail.com>
2016-11-09 14:24:56 +0100

    solution

--- kol2_gr2.py
+++ kol2_gr2.py
@@ -14,3 +14,84 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+class Course(object):
+	name = ''
+	
+	def __init__(self, name):
+		self.name = name
+
+
+class Grade(object):
+	value = ''
+	course = Course
+
+	def __init__(self, value, course):
+		self.value = value
+		self.course = course
+
+
+class Student(object):
+	firstname = ''
+	lastname = ''
+	grades = []
+	attendances = []
+
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname
+		self.lastname = lastname
+	
+	def addGrade(self, grade):
+		self.grades.append(grade)
+
+	def getAvg(self):
+		gradesSum = 0.
+		gradesCnt = len(self.grades)
+
+		for grade in self.grades:
+			gradesSum += grade.value
+
+
+		if gradesCnt:
+			return gradesSum/gradesCnt
+		return 0
+
+	def getCourseAvg(self, course):
+		gradesSum = 0.
+		gradesCnt = 0
+
+		for grade in self.grades:
+			if grade.course.name == course.name:
+				gradesSum += grade.value
+				gradesCnt += 1
+
+		if gradesCnt:
+			return gradesSum/gradesCnt
+		return 0
+
+	def add_attendance(self, present):
+		self.attendances.append(present)
+
+	def get_attendance_stat(self):
+		return sum(self.attendances)/len(self.attendances)
+
+math = Course("Math")
+physics = Course("Physics")
+
+student_a = Student("Tomasz", "Abacki")
+student_b = Student("Adam", "Babacki")
+
+student_a.addGrade(Grade(5, math))
+student_a.addGrade(Grade(3, math))
+student_a.addGrade(Grade(5, physics))
+
+student_a.add_attendance(1.)
+student_a.add_attendance(1.)
+student_a.add_attendance(1.)
+student_a.add_attendance(0.)
+
+print "Student "+student_a.firstname+" "+student_a.lastname
+print "Grades avg: "+str(student_a.getAvg())
+print "Grades for course "+math.name+": "+str(student_a.getCourseAvg(math))
+print "Grades for course "+physics.name+": "+str(student_a.getCourseAvg(physics))
+print "Attendance "+str(student_a.get_attendance_stat())
